09e1e7c56db877843b91ed2350629419d68cfc86
VHSSatyendra <vhsdesndev@gmail.com>
2026-03-31 15:58:30 +0530

    first commit 04

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import.meta.env.VITE_API_URL;
-import.meta.env.VITE_API_TOKEN;
 
 // https://vite.dev/config/
 export default defineConfig({
